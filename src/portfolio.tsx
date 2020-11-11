50237f656f108edc2b87ddaaab82297ba5daf53e
baehongjun0212 <sembuat12@naver.com>
2020-11-11 15:05:53 +0900

    Protfolio.tsx File Update

--- src/portfolio.tsx
+++ src/portfolio.tsx
@@ -1,10 +1,10 @@
 import emoji from 'react-easy-emoji';
 
 export const greeting = {
-	username: 'Username',
-	title: 'Hello I\'m Username',
-	subTitle: emoji("subtitle ❤"),
-	role: "Your role",
+	username: 'Hong Jun Bae',
+	title: 'Hello I\'m Hong Jun',
+	subTitle: emoji("성실과 열정으로 내일의 가능성을 여는 개발자, 배홍준입니다 :)"),
+	role: "I'm noob student developer.",
 	resumeLink: "Your resume link... google drive or something else",
 	// IMPORTANT
 	// If you don't want to show this, change view to false. DO NOT DELETE!
@@ -14,31 +14,31 @@ export const greeting = {
 export const socialMediaLinks = {
 	// IMPORTANT
 	// if you don't have, change value to blank or false!! DO NOT DELETE!
-	github: 'Your github link',
+	github: 'https://github.com/baehongjun0212',
 	linkedin: 'Your linkedin link',
-	email: 'your email link',
-	facebook: 'Your facebook link',
+	email: '<A href="mailto:sembuat12@gmail.com">메일 전송</a>',
+	facebook: 'https://www.facebook.com/baehongjun0212',
 	twitter: "Your twitter link",
-	instagram: "Your instagram link",
+	instagram: "https://www.instagram.com/b.hhong/",
 	medium: 'Your medium link',
 	stackoverflow: 'Your stackoverflow link'
 };
 
 export const skills = {
 	title: emoji("I Can Do ⚡"),
-	subTitle: "subtitle for skill",
+	subTitle: "　",
 	describeSkills: [
-		emoji("✔ describe first"),
-		emoji("✔ describe second"),
-		emoji("✔ describe third"),
-		emoji("✔ describe forth"),
-		emoji("✔ describe fifth"),
+		emoji("✔ Java 8을 사용한 공지사항 푸시 알림 프로젝트 진행"),
+		emoji("✔ Python 3을 사용한 학생 정보 GUI 프로젝트 진행"),
+		emoji("✔ C & C++을 사용한 얼굴 감지 마스킹 프로젝트 진행"),
+		emoji("✔ R과 Google Maps를 사용한 코로나 확진자 위치 시각화 진행"),
+		emoji("✔ Flutter을 사용한 공지사항 푸시 알림 하이브리드 앱 개발"),
 	],
 	// IMPORTANT
 	// You need to find className for font-awesome icon
 	// Go to https://fontawesome.com/ and search icon
 	lists: [
-		{fontAwesome: "fab fa-html5", text: "HTML5", proficiency: 80},
+		{fontAwesome: "fab fa-html5", text: "HTML5", proficiency: 80}, 
 		{fontAwesome: "fab fa-css3-alt", text: "CSS3", proficiency: 70},
 		{fontAwesome: "fab fa-js", text: "JS", proficiency: 70},
 		{fontAwesome: "fab fa-sass", text: "Sass", proficiency: 30},
